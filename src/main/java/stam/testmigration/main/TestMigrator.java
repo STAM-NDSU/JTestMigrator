99be95540ad405703be74cf0640f0f2f9acf645c
Ajay Kumar Jha <kajay.jha@gmail.com>
2023-07-13 10:57:48 -0500

    remove a class

--- src/main/java/stam/testmigration/main/TestMigrator.java
+++ src/main/java/stam/testmigration/main/TestMigrator.java
@@ -1,13 +1,10 @@
 package stam.testmigration.main;
 
-import stam.testmigration.setup.ConfigCreator;
 import stam.testmigration.setup.ConfigurationRetriever;
 import stam.testmigration.setup.SetupTargetApp;
 
 public class TestMigrator {
     public static void main(String[] args) {
-        new ConfigCreator().createConfigFile();
-
         ConfigurationRetriever configRet = new ConfigurationRetriever();
         configRet.getConfigurations();
         configRet.getSourceTargetDirs();
